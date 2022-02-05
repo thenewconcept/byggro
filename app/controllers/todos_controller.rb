abca5b62f7c5a6c10fd81899dd89713a02fd14bc
Antony Sastre <antony@thenewconcept.com>
2022-02-05 21:12:27 +0100

    Hotwiring todo crud processes.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,6 +4,33 @@ class TodosController < ProtectedController
     @todo = @checklist.todos.new
   end
 
+  def edit
+    @checklist = Checklist.find(params[:checklist_id])
+    @todo      = @checklist.todos.find(params[:id])
+  end
+
+  def create
+    @checklist = Checklist.find(params[:checklist_id])
+    @todo      = @checklist.todos.new(todo_params)
+
+    if @todo.save!
+      redirect_to project_url(@checklist.project)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @checklist = Checklist.find(params[:checklist_id])
+    @todo      = @checklist.todos.find(params[:id])
+
+    if @todo.update!(todo_params)
+      redirect_to project_url(@checklist.project)
+    else
+      render :edit
+    end
+  end
+
   def create
     @checklist = Checklist.find(params[:checklist_id])
     @todo      = @checklist.todos.new(todo_params)
