b580b196fb8ce11ca5b96369aa84d7fd7d454b46
Antony Sastre <antony@thenewconcept.com>
2022-02-05 17:37:48 +0100

    Update 20220205120049_create_todos.rb

--- db/migrate/20220205120049_create_todos.rb
+++ db/migrate/20220205120049_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[7.0]
   def change
     create_table :todos do |t|
       t.references :checklist, null: false, foreign_key: true
-      t.boolean :completed
+      t.boolean :completed, null: false, default: false
       t.string :description
 
       t.timestamps
