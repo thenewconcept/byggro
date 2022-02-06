d6124861b478ef9e0e43e942f3a97dea8e1ae7d1
Antony Sastre <antony@thenewconcept.com>
2022-02-06 12:29:04 +0100

    Admin has manager roles.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+
+  def is_manager?
+    (is_manager || is_admin)
+  end
 end
