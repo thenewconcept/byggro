fca7889bebb7b5d3442dbc9257309da5d6e689b5
Antony Sastre <antony@thenewconcept.com>
2022-02-23 15:11:45 +0100

    Spec tests.

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'user login' do
+  it 'works with a valid user' do
+    user = create(:user, email: 'jinx@doe.com', password: '12345678')
+    visit root_path
+    fill_in :email, with: 'jinx@doe.com'
+    fill_in :password, with: '12345678'
+    click_button 'Logga in'
+    expect(page).to have_content('Signed in successfully.')
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,55 +1,27 @@
-# See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'pundit/rspec'
+require 'capybara/dsl'
+
 RSpec.configure do |config|
+  def fill_field(selector, text)
+    text.split('').each { |c| find_field(selector).native.send_keys(c) }
+  end
+
+  config.include Capybara::DSL
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
-
-=begin
-  # This allows you to limit a spec run to individual examples or groups
-  # you care about by tagging them with `:focus` metadata. When nothing
-  # is tagged with `:focus`, all examples get run. RSpec also provides
-  # aliases for `it`, `describe`, and `context` that include `:focus`
-  # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
   config.filter_run_when_matching :focus
-
-  # Allows RSpec to persist some state between runs in order to support
-  # the `--only-failures` and `--next-failure` CLI options. We recommend
-  # you configure your source control system to ignore this file.
   config.example_status_persistence_file_path = "spec/examples.txt"
-
   config.disable_monkey_patching!
 
-  # Many RSpec users commonly either run the entire suite or an individual
-  # file, and it's useful to allow more verbose output when running an
-  # individual spec file.
-  if config.files_to_run.one?
-    # Use the documentation formatter for detailed output,
-    # unless a formatter has already been configured
-    # (e.g. via a command-line flag).
-    config.default_formatter = "doc"
-  end
+  config.default_formatter = "doc" if config.files_to_run.one?
 
-  # Print the 10 slowest examples and example groups at the
-  # end of the spec run, to help surface which specs are running
-  # particularly slow.
   config.profile_examples = 10
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
   config.order = :random
 
-  # Seed global randomization in this process using the `--seed` CLI option.
-  # Setting this allows you to use `--seed` to deterministically reproduce
-  # test failures related to randomization by passing the same `--seed` value
-  # as the one that triggered the failure.
   Kernel.srand config.seed
-=end
 end
